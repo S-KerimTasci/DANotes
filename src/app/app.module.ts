00c9bc3ec5706e61d19b434f6a6d4953338ec50e
Kerim <kerim.tasci@yahoo.de>
2023-12-23 10:09:48 +0100

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { NoteListComponent } from './note-list/note-list.component';
+import { NoteComponent } from './note-list/note/note.component';
+import { FormsModule } from '@angular/forms';
+import { AddNoteDialogComponent } from './add-note-dialog/add-note-dialog.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NoteListComponent,
+    NoteComponent,
+    AddNoteDialogComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    provideFirebaseApp(() => initializeApp({"projectId":"danotes-bbb4c","appId":"1:180025876302:web:50431870a3870e7e8ec81f","storageBucket":"danotes-bbb4c.appspot.com","apiKey":"AIzaSyAhzYwcWW4-ulrgDm-qZeyZFcneOTbgIp8","authDomain":"danotes-bbb4c.firebaseapp.com","messagingSenderId":"180025876302"})),
+    provideFirestore(() => getFirestore()),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
